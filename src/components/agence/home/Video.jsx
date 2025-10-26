4903bbfe2fd64373b57c57b6193656f6249848e2
vishu2672003 <rc071649@gmail.com>
2025-10-26 18:14:16 +0530

    Update video source to use Vimeo link

--- src/components/agence/home/Video.jsx
+++ src/components/agence/home/Video.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Video = () => {
   return (
     <div className='h-full w-full'>
-      <video className='h-full w-full object-cover' autoPlay loop muted src="../../../public/Home.mp4"></video>
+      <video className='h-full w-full object-cover' autoPlay loop muted src="https://download-video-ak.vimeocdn.com/v3-1/playback/36bc59b8-6671-4358-abc2-15555fc6ae59/69496b2d?__token__=st=1761482601~exp=1761486201~acl=%2Fv3-1%2Fplayback%2F36bc59b8-6671-4358-abc2-15555fc6ae59%2F69496b2d%2A~hmac=fa970b05203b1d61e6800e94535bf56383abd9f8a51ddb3b654298a58b8ce10d&r=dXMtZWFzdDE%3D"></video>
     </div>
   )
 }
