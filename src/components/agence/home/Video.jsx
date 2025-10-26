b12c2431452195d70bf8410f5e763da6f699169b
vishu2672003 <rc071649@gmail.com>
2025-10-26 18:06:28 +0530

    Update video source to local Home.mp4 file

--- src/components/agence/home/Video.jsx
+++ src/components/agence/home/Video.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Video = () => {
   return (
     <div className='h-full w-full'>
-      <video className='h-full w-full object-cover' autoPlay loop muted src="https://download-video-ak.vimeocdn.com/v3-1/playback/36bc59b8-6671-4358-abc2-15555fc6ae59/69496b2d?__token__=st=1761451747~exp=1761455347~acl=%2Fv3-1%2Fplayback%2F36bc59b8-6671-4358-abc2-15555fc6ae59%2F69496b2d%2A~hmac=7ddcb796bb37bcc6f26ed033f71da8205b531573a28f09df37410c1aabc4ee66&r=dXMtZWFzdDE%3D"></video>
+      <video className='h-full w-full object-cover' autoPlay loop muted src="../../../public/Home.mp4"></video>
     </div>
   )
 }
